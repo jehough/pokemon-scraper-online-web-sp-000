2dd64301f1c3ad36539b3185baf027eac2978953
Jim Hough <teachlife@outlook.com>
2019-05-30 00:53:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,4 +14,16 @@ class Pokemon
     db.execute(sql, name, type)
   end
 
+  def self.new_from_db(row, db)
+    pok_id = row[0]
+    pok_name = row[1]
+    pok_type = row[2]
+    pokemon = self.new (name: pok_name, type: pok_type, id: pok_id, db: db)
+  end  
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = ?"
+    db.execute(sql, id).map do |row|
+      self.new_from_db(row, db)
+    end.first
+  end
 end
